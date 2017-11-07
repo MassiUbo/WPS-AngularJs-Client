d159a7e482306c48a36a8387f2ff7e79236fecd2
massi <mr.hadjsaid@hotmail.fr>
2017-11-07 14:19:05 +0100

    Changement V Angular et Ajout process JST:diff

--- js/myAngularApp.js
+++ js/myAngularApp.js
@@ -1,26 +1,52 @@
 
-var myApp= angular.module("angularApp",[]);    //declaration du module
-
-myApp.controller("myController", function($scope){
-$scope.textname = ""; 
-
-$scope.items = [{
-id: 1,
-label: 'degree',
-subItem: [{ name: 'ser1-1' },
-        { name: '4' },{ name: '5' },
-        { name: '6' }]
-}, {
-id: 2,
-label: 'geoserver',
-subItem: [{ name: 'JST:difference', data: [{ var1: 'text', desc: 'type your name Please' },{ var1: 'text' , desc: 'how old are you'},{ var1: 'radio' , desc: ' wa cliki 3la dik dawara ' }] },
-          { name: 'ser2-2', data: [{ var1: 'radio' },{ var2: 'image' },{ var3: 'text' }] },
-          { name: 'ser2-3', data: [{ var1: 'checkbox' },{ var2: 'image' },{ var3: 'radio' }] },
-          { name: 'ser2-4', data: [{ var1: 'text' },{ var2: 'radio' }] }
-          ]
-}];
-
-$scope.selected = null;
-$scope.select = null;
-
+var myApp = angular.module("angularApp", [])   //declaration du module
+myApp.controller("myController", function ($scope) {
+    
+    $scope.txtname = ""; 
+    $scope.items = [{
+      id: 1,
+      label: 'degree',
+      subItem: [{ name: 'process1',
+                 data: [{ var1: 'text', desc: 'Input' },
+                        { var1: 'text' , desc: 'voila'},
+                        { var1: 'radio' , desc: ' rond' }] ,
+                 
+                Type : [{var1 :'text' , desc :'text'},
+                        {var1 : 'text' , desc : 'reference'},
+                        {var1 : 'text' , desc :'Subproces'}] ,
+                 
+                 app : [{ var1: 'text', desc: 'application/json' } ,
+                        { var1: 'text', desc: 'application/wkt' } ,
+                        { var1: 'text', desc: 'application/gml-3.1.1' }        
+                       ] ,
+                },
+                  ]
+    },     
+                    {
+      id: 2,
+      label: 'geoserver',
+      subItem: [{ name: 'JST:difference',
+                 data: [{ var1: 'text', desc: 'Input' },
+                        { var1: 'text' , desc: 'how old are you'},
+                        { var1: 'radio' , desc: ' rond' }] ,
+                 
+                Type : [{var1 :'text' , desc :'text'},
+                        {var1 : 'text' , desc : 'reference'},
+                        {var1 : 'text' , desc :'Subproces'}] ,
+                 
+                 app : [{ var1: 'text', desc: 'application/json' } ,
+                        { var1: 'text', desc: 'application/wkt' },
+                        { var1: 'text', desc: 'application/gml-3.1.1' } , ] ,
+                        
+                  input1: [{ var1: 'text', desc: 'a* - geometry ' },
+                        { var1: 'text' , desc: 'First input geometry '},
+                        ] ,
+                 
+                 input2: [{ var1: 'text', desc: 'b* - geometry ' },
+                        { var1: 'text' , desc: 'Second input geometry '},
+                        ] ,
+                },]
+    }];
+    $scope.selected = null;
+    $scope.select = null;
 });
