d928b59bcc67807b55e24573ca2d963dc9309204
massi <mr.hadjsaid@hotmail.fr>
2018-01-05 23:44:32 +0100

    WPS Assemble 05 01

---
+++ WPS Assemblee 05 01/js/myAngularApp.js
@@ -0,0 +1,162 @@
+
+var myApp = angular.module("angularApp", [])           //declaration du module
+
+
+myApp.controller("myController", function ($scope) {   // controller 
+
+        //$scope.txtname = ""; 
+        $scope.visible;
+        $scope.txtname = "";
+        
+        // pour affichage process
+        $scope.affichage = function () {
+                $scope.visible = true;
+                return visible;
+        }
+
+        $scope.items = [{
+                id: 1,
+                label: 'http://geoprocessing.demo.52north.org:8080/wps/WebProcessingService',
+        },
+        {
+                id: 2,
+                label: 'https://geobretagne.fr/geoserver/ows?',
+
+        },
+        {
+                id: 3,
+                label: 'http://zoo-project.org/zoo',
+
+        }
+
+        ];
+
+        $scope.selected = null;
+        $scope.select = null;
+
+});
+
+// fonction de configuration 
+function config() {
+
+        //document.getElementById("processDescription").style.display = "none";
+        document.getElementById('wpsexe').style.display = 'block';
+        //document.getElementById('divName').style.visibility='hidden';
+        //document.getElementById("divSlider").style.display = "block";
+        document.getElementById("divSlider").innerHTML = "<br> Inputs: <br> <br>";
+        var selectedIndex = $("select[id='wpsfav'] option:selected").index();
+
+        var inputnbr = 0;
+
+        if (selectedIndex != 0) {
+                for (var indexInput in processDescription.processOffering.process.inputs) {
+                        var n = 0;
+                        while ((n < processDescription.processOffering.process.inputs[indexInput].maxOccurs) && document.getElementById("notused" + indexInput + n).checked) {
+
+                                var ok = document.getElementById("myInputs[" + indexInput + n + "]").value;
+                                inputnbr = inputnbr + 1;
+                                n = n + 1;
+                        }
+                }
+
+                var i = 0;
+                var listeInputs = "";
+
+                while (i < nbinputfavwps[selectedIndex - 1]) {
+                        if ($('#wpsmassi').length > 0) {
+                                //complex data
+                                //	var d = document.wpsmassi.wfsfav;
+
+                                if (inputValue[selectedIndex - 1][i].indexOf("^") >= 0) {
+
+                                        var index = inputValue[selectedIndex - 1][i].indexOf('^');
+                                        strOut = inputValue[selectedIndex - 1][i].substr(index + 1);
+                                        strOutNS = inputValue[selectedIndex - 1][i].substr(0, index);
+                                }
+
+                                adresseWps = adrfavwps[selectedIndex - 1];
+                                identifier = idfavwps[selectedIndex - 1];
+
+                                if ($('#1' + i + '0').length > 0) {
+
+                                        if (isFixed[selectedIndex - 1][i] == true) {
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + inputValue[selectedIndex - 1][i] + "<br>";
+                                        }
+
+                                        //if user
+                                        else {
+                                                var selectedIndex = $("select[id='wpsfav'] option:selected").index();
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + "<textarea type='text' name='text" + i + "'  id='text" + i + "'  style='width:250px;height:15px;' value='" + inputValue[selectedIndex - 1][i] + "'>" + inputValue[selectedIndex - 1][i] + "</textarea><br>";
+
+                                                /*	if ($('#wfsfav').length > 0) {
+                                                                document.getElementById("divSlider").innerHTML += '<form name="wfsfavform2" id="wfsfavform2" style="display:block"> <SELECT NAME="wfsf' + i + '" id="wfsf' + i + '" onChange="wfsFav3(' + i + ');">		<OPTION VALUE="">Choisir un favori WFS<OPTION VALUE="https://geobretagne.fr/geoserver/ows?^dreal_b:stationnement_littoral">https://geobretagne.fr/geoserver//ows?^dreal_b:stationnement_littoral<OPTION VALUE="http://geoserver.ics.perm.ru/geoserver/ows?^topp:tasmania_state_boundaries">http://geoserver.ics.perm.ru/geoserver//ows?^topp:tasmania_state_boundaries<OPTION VALUE="http://geoserver.ics.perm.ru/geoserver/ows?^topp:tasmania_roads">http://geoserver.ics.perm.ru/geoserver//ows?^topp:tasmania_roads<OPTION VALUE="http://geoserver.ics.perm.ru/geoserver/ows?^topp:states">http://geoserver.ics.perm.ru/geoserver//ows?^topp:states</SELECT>  </form>';
+                                                        }*/
+                                        }
+
+                                        listeInputs += idInputs[selectedIndex - 1][i] + "=" + inputValue[selectedIndex - 1][i] + ";";
+                                }
+
+                                else {
+                                        //si pas favori wps selectionné
+                                        //alert("4");
+                                        if ($('#1' + i + '0').length > 0) {
+                                                alert(isFixed[selectedIndex - 1][i]);
+                                                if (isFixed[selectedIndex - 1][i] == true) {
+                                                        //alert("1");
+                                                        document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + inputValue[selectedIndex - 1][i] + "<br>";
+                                                        document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][indexInput] + "<br>" + ok + "<br>";
+
+                                                }
+                                                //if user
+                                                else {
+
+                                                        /*if ((ok.indexOf(adressewfs) >= 0)) {
+                                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + "<textarea type='text' name='text" + i + "'  id='text" + i + "'  style='width:250px;height:15px;' value='" + inputValue[selectedIndex - 1][i] + "'>" + inputValue[selectedIndex - 1][i] + "</textarea><br>";
+                                                        } */
+                                                        //else {
+                                                        document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + "<textarea type='text' name='text" + i + "'  id='text" + i + "'  style='width:250px;height:15px;' value='" + inputValue[selectedIndex - 1][i] + "'>" + inputValue[selectedIndex - 1][i] + "</textarea><br>";
+
+                                                        //	}
+                                                }
+                                        }
+                                }
+                        }
+                        else {
+
+                                if ($('#1' + i + '0').length > 0) {
+
+                                        if (isFixed[selectedIndex - 1][i] == false) {
+
+                                                //alert("2");
+                                                listeInputs += idInputs[selectedIndex - 1][i] + "=" + inputValue[selectedIndex - 1][i] + ";";
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex - 1][i] + "<br>" + "<textarea type='text' name='text" + i + "'  id='text" + i + "'  style='width:250px;height:15px;' value='" + inputValue[selectedIndex][i] + "'>" + inputValue[selectedIndex][i] + "</textarea><br>";
+
+                                        }
+                                        else {
+                                                //alert("5");
+                                                listeInputs += idInputs[selectedIndex][i] + "=" + inputValue[selectedIndex][i] + ";";
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex][i] + "<br>" + inputValue[selectedIndex][i] + "<br>";
+                                        }
+                                }
+                                //if user
+                                else {
+
+                                        if ($('#1' + i + '0').length > 0) {
+                                                //alert("6");
+                                                listeInputs += idInputs[selectedIndex][i] + "=" + inputValue[selectedIndex][i] + ";";
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex][i] + "<br>" + "<textarea type='text' name='myInputs[" + i + "]'  id='myInputs[" + i + "]'  style='width:250px;height:15px;' value='" + inputValue[selectedIndex][i] + "'></textarea><br>";
+                                        }
+                                        else {
+                                                //alert("7");
+                                                listeInputs += idInputs[selectedIndex][i] + "=" + inputValue[selectedIndex][i] + ";";
+                                                document.getElementById("divSlider").innerHTML += idInputs[selectedIndex][i] + "<br>" + inputValue[selectedIndex][i] + "<br>";
+
+                                        }
+                                }
+                        }
+                        i = i + 1;
+                }
+        }
+
+}
+
